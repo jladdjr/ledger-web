67de909cdd20b5660b5a9da114619c68dd3abe78
Jim Ladd <jladdjr@gmail.com>
2022-07-14 19:04:56 -0500

    drop even the suggestion of a literal secret key

--- ledgerweb/ledgerweb/settings.py
+++ ledgerweb/ledgerweb/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%mzg)2@=xk(p2=6dqgmahqid8e!af108y+h2)@dti5l0e^g3ag'
+# SECRET_KEY = <some very long string>
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
