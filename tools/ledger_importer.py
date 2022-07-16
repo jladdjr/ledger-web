ec8ea9c273dc7e84db483f7c29cae2475f4b3df7
Jim Ladd <jladdjr@gmail.com>
2022-07-16 17:05:50 -0500

    allow month, day in dates to be one or two digits long

--- tools/ledger_importer.py
+++ tools/ledger_importer.py
@@ -166,13 +166,13 @@ def _form_transaction(text: list[str]):
     # TODO: Currently ignores setting effective date
     #       e.g. 2022/02/25=2022/03/07
     date_text = text[0].split()[0]
-    res = re.match(r'^(\d{4}/\d{2}/\d{2})(=\d{4}/\d{2}/\d{2})?', date_text)
+    res = re.match(r'^(\d{4}/\d{1,2}/\d{1,2})(=\d{4}/\d{1,2}/\d{1,2})?', date_text)
     if not res:
         raise MalformedTransaction(f'Expected date, found {text[0]}')
     date = res.group(1)
 
     # parse description
-    res = re.match(r'^\d{4}/\d{2}/\d{2}(=\d{4}/\d{2}/\d{2})?\s+(.*)\s*$', text[0])
+    res = re.match(r'^\d{4}/\d{1,2}/\d{1,2}(=\d{4}/\d{1,2}/\d{1,2})?\s+(.*)\s*$', text[0])
     if not res:
         raise MalformedTransaction(f'Could not find description, found: {text[0]}')
     description = res.group(2)
